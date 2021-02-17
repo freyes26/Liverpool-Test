75a9496f875459285138cf0590135dbff4b4c9e6
Francisco Reyes Lira <franreyes.26041996@gmail.com>
2021-02-16 22:19:03 -0600

    project structure

---
+++ app/src/main/java/com/liverpool/test/liverpooltest/ui/DashboardFragment.kt
@@ -0,0 +1,33 @@
+package com.liverpool.test.liverpooltest.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.liverpool.test.liverpooltest.R
+import com.liverpool.test.liverpooltest.viewModel.DashboardViewModel
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+                ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(
+                    DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
